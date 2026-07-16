864bb714964042a751cebf86a6ca71759fc6ad73
dandyii <dandynugroho901@gmail.com>
2026-07-16 16:04:02 +0700

    .

--- app/train_model.py
+++ app/train_model.py
@@ -8,6 +8,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import NearestNeighbors
+from xgboost import XGBClassifier
 from sklearn.metrics import (
     accuracy_score, precision_score, recall_score, f1_score, roc_auc_score, average_precision_score,
     confusion_matrix, roc_curve, precision_recall_curve
